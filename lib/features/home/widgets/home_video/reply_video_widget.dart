9fa8e9fdfb198c08eebcf3b0e062880b0b9e8195
Michael Dadzie <dadziemikke2@gmail.com>
2024-09-05 07:46:20 +0000

    fix: fix video playing in the background after exiting reply video widget

--- lib/features/home/widgets/home_video/reply_video_widget.dart
+++ lib/features/home/widgets/home_video/reply_video_widget.dart
@@ -1,27 +1,20 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:socialverse/core/domain/models/post_model.dart';
 import 'package:socialverse/export.dart';
 import 'package:socialverse/features/home/providers/reply_provider.dart';
-
-import '../../utils/video_sheet.dart';
-import 'last_page_gradient.dart';
+import 'package:socialverse/features/home/utils/video_sheet.dart';
+import 'package:socialverse/features/home/widgets/home_video/reply_play_button.dart';
 
 class ReplyVideoWidget extends StatefulWidget {
   final Posts video;
   final PageController pageController;
-  final int pIdx;
-  final bool isLastPage;
-  final int parentIdx;
+  final int postIndex;
+  final int parentIndex;
   final bool isInit;
   const ReplyVideoWidget({
     super.key,
     required this.video,
     required this.pageController,
-    required this.pIdx,
-    required this.isLastPage,
-    required this.parentIdx,
+    required this.postIndex,
+    required this.parentIndex,
     required this.isInit,
   });
 
@@ -30,7 +23,7 @@ class ReplyVideoWidget extends StatefulWidget {
 }
 
 class _ReplyVideoWidgetState extends State<ReplyVideoWidget> {
-  // late VideoPlayerController videoPlayerController;
+  ReplyProvider? _reply;
 
   @override
   void initState() {
@@ -38,17 +31,27 @@ class _ReplyVideoWidgetState extends State<ReplyVideoWidget> {
     super.initState();
   }
 
-  /*Initializes video controller for the horizontal feed */
   initializeVideo() async {
-    final reply = Provider.of<ReplyProvider>(context, listen: false);
-    await reply.initializedVideoPlayer();
-    reply.index = widget.pIdx;
+    _reply = Provider.of<ReplyProvider>(context, listen: false);
+    await _reply?.initializedVideoPlayer();
+    _reply?.index = widget.postIndex;
   }
 
   @override
-  /*Everything here on out is controlled through the reply provider including
-  the horizontal video controllers ie reply.videoController()
-  */
+  void didChangeDependencies() {
+    _reply = Provider.of<ReplyProvider>(context);
+    super.didChangeDependencies();
+  }
+
+  @override
+  void dispose() {
+    _reply?.controllers.forEach((key, value) async {
+      value.dispose();
+    });
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final reply = Provider.of<ReplyProvider>(context);
     final home = Provider.of<HomeProvider>(context);
@@ -64,16 +67,17 @@ class _ReplyVideoWidgetState extends State<ReplyVideoWidget> {
           controller: widget.pageController,
           physics: CustomBouncingScrollPhysics(),
           itemCount: reply.posts.length,
-          itemBuilder: (context, hindex) {
-            bool isInit2 = reply.videoController(hindex)!.value.isInitialized;
+          itemBuilder: (context, index) {
+            bool isReplyInit =
+                reply.videoController(index)!.value.isInitialized;
             return GestureDetector(
               onDoubleTap: () {
                 reply.handleDoubleTap();
 
-                if (reply.posts[hindex].upvoted == false) {
-                  reply.posts[hindex].upvoted = true;
-                  reply.posts[hindex].upvoteCount++;
-                  home.postLikeAdd(id: reply.posts[hindex].id);
+                if (reply.posts[index].upvoted == false) {
+                  reply.posts[index].upvoted = true;
+                  reply.posts[index].upvoteCount++;
+                  home.postLikeAdd(id: reply.posts[index].id);
                 }
 
                 Timer(Duration(seconds: 1), () => home.isLiked = false);
@@ -109,28 +113,28 @@ class _ReplyVideoWidgetState extends State<ReplyVideoWidget> {
                   ),
                   builder: (context) {
                     return VideoSheet(
-                      isUser: reply.posts[hindex].username != prefs_username,
+                      isUser: reply.posts[index].username != prefs_username,
                       isFromFeed: true,
-                      video_id: reply.posts[hindex].id,
+                      video_id: reply.posts[index].id,
                       category_name: '',
                       category_count: 0,
                       category_id: 0,
                       category_photo: '',
                       category_desc: '',
-                      title: reply.posts[hindex].title,
-                      video_link: reply.posts[hindex].videoLink,
-                      current_index: widget.parentIdx,
+                      title: reply.posts[index].title,
+                      video_link: reply.posts[index].videoLink,
+                      current_index: widget.parentIndex,
                     );
                   },
                 );
               },
               onTap: () {
-                if (reply.videoController(hindex)!.value.isPlaying) {
+                if (reply.videoController(index)!.value.isPlaying) {
                   reply.isPlaying = false;
-                  reply.videoController(hindex)!.pause();
+                  reply.videoController(index)!.pause();
                 } else {
                   reply.isPlaying = true;
-                  reply.videoController(hindex)!.play();
+                  reply.videoController(index)!.play();
                 }
               },
               child: Stack(
@@ -144,50 +148,29 @@ class _ReplyVideoWidgetState extends State<ReplyVideoWidget> {
                     child: Stack(
                       alignment: Alignment.center,
                       children: [
-                        if (!widget.isLastPage) ...[
-                          CustomNetworkImage(
-                            height: cs().height(context),
-                            width: cs().width(context),
-                            imageUrl: reply.posts[hindex].thumbnailUrl,
-                            isLoading: true,
-                          ),
-                        ],
-                        if (isInit2 && !widget.isLastPage) ...[
+                        CustomNetworkImage(
+                          height: cs().height(context),
+                          width: cs().width(context),
+                          imageUrl: reply.posts[index].thumbnailUrl,
+                          isLoading: true,
+                        ),
+                        if (isReplyInit) ...[
                           SizedBox.expand(
                             child: FittedBox(
                               fit: BoxFit.cover,
                               child: SizedBox(
                                   width: reply
-                                      .videoController(hindex)!
+                                      .videoController(index)!
                                       .value
                                       .size
                                       .width,
                                   height: reply
-                                      .videoController(hindex)!
+                                      .videoController(index)!
                                       .value
                                       .size
                                       .height,
                                   child: VideoPlayer(
-                                      reply.videoController(hindex)!)),
-                            ),
-                          ),
-                        ],
-                        if (widget.isLastPage) ...[
-                          LastPageGradient(
-                            isInit: isInit2,
-                            child: SizedBox(
-                              width: reply
-                                  .videoController(hindex)!
-                                  .value
-                                  .size
-                                  .width,
-                              height: reply
-                                  .videoController(hindex)!
-                                  .value
-                                  .size
-                                  .height,
-                              child:
-                                  VideoPlayer(reply.videoController(hindex)!),
+                                      reply.videoController(index)!)),
                             ),
                           ),
                         ],
@@ -201,10 +184,8 @@ class _ReplyVideoWidgetState extends State<ReplyVideoWidget> {
                             color: Theme.of(context).hintColor,
                             label: 'Video Saved',
                           ),
-                        if (!widget.isLastPage) ...[
-                          video.isViewMode ? shrink : InfoSideBar(),
-                        ],
-                        if (!widget.isLastPage) PlayButton(),
+                        video.isViewMode ? shrink : InfoSideBar(),
+                        ReplyPlayButton(),
                         if (home.isLiked) ...[
                           Positioned(
                             left: home.tapPosition.dx - 75,
@@ -221,9 +202,7 @@ class _ReplyVideoWidgetState extends State<ReplyVideoWidget> {
                             ),
                           )
                         ],
-                        if (!widget.isLastPage) ...[
-                          video.isViewMode ? shrink : HomeSideBar(),
-                        ],
+                        video.isViewMode ? shrink : HomeSideBar(),
                         HomeVideoProgressIndicator(),
                       ],
                     ),
@@ -231,43 +210,12 @@ class _ReplyVideoWidgetState extends State<ReplyVideoWidget> {
                 ],
               ),
             );
-
-            // bool isInit = reply.videoController(hindex)!.value.isInitialized;
-            // return VideoInterface(video: home.posts[index], indx: hindex);
-            // return isInit
-            //     ? VideoPlayer(
-            //         reply.videoController(hindex)!,
-            //       )
-            //     : Container();
           },
         ),
       ],
     );
   }
 }
-
-// class CustomNoOverscrollPhysics extends ClampingScrollPhysics {
-//   CustomNoOverscrollPhysics({ScrollPhysics? parent})
-//       : super(parent: parent);
-//   @override
-//   CustomNoOverscrollPhysics applyTo(ScrollPhysics? ancestor) {
-//     return CustomNoOverscrollPhysics(parent: buildParent(ancestor));
-//   }
-
-//   @override
-//   double applyBoundaryConditions(ScrollMetrics position, double value) {
-//     // Allow the pixels to reflect beyond minScrollExtent but prevent visual scroll beyond the extent
-//     if (value < position.minScrollExtent) {
-//       // Return 0.0 to prevent actual scrolling, but allow the position.pixels to reflect the underflow
-//       // reply.prevIndex = 0;
-//       return value - position.minScrollExtent;
-//     } else if (value > position.maxScrollExtent) {
-//       // Prevent scrolling beyond maxScrollExtent
-//       return value - position.maxScrollExtent;
-//     }
-//     return 0.0; // No boundary conditions apply, normal scroll
-//   }
-// }
 
 class CustomBouncingScrollPhysics extends BouncingScrollPhysics {
   final double nFrictionFactor;
