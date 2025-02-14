138552d1ca2a628a1340eb901cc47e4607b20bc3
Ayan Mahanta <112053665+Ayan20april@users.noreply.github.com>
2025-02-14 12:57:43 +0530

    Update user_search_list.dart

--- lib/features/search/widgets/search/user_search_list.dart
+++ lib/features/search/widgets/search/user_search_list.dart
@@ -5,83 +5,83 @@ class UserSearchList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final userProfileProvider =
-        Provider.of<UserProfileProvider>(context, listen: false);
     return Consumer<SearchProvider>(
-      builder: (_, __, ___) {
-        if (__.search.text.isNotEmpty && __.user_search.isEmpty) {
-          return Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.search,
-                size: 200,
-                color: Theme.of(context).indicatorColor,
+      builder: (_, provider, __) {
+        if (provider.search.text.isNotEmpty && provider.user_search.isEmpty) {
+          return Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.search,
+                    size: 200,
+                    color: Theme.of(context).indicatorColor,
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    "It seems we can't find the user\nyou're looking for. Try another search.",
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ],
               ),
-              Text(
-                "It seems we can't find the user\n you're looking for. Try another\n search.",
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-            ],
+            ),
           );
         }
+
         return ListView.builder(
           scrollDirection: Axis.vertical,
-          padding: const EdgeInsets.only(
-            left: 20,
-            right: 20,
-            bottom: 20,
-          ),
-          itemCount: __.user_search.length,
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          itemCount: provider.user_search.length,
           itemBuilder: (context, index) {
-            return GestureDetector(
-              onTap: () {
-                Navigator.of(context).pushNamed(
-                  UserProfileScreen.routeName,
-                  arguments: UserProfileScreenArgs(
-                    username: __.user_search[index].username,
-                  ),
-                );
-              },
-              child: Container(
-                height: 65,
-                margin: const EdgeInsets.symmetric(vertical: 5),
-                width: cs().width(context),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    CustomCircularAvatar(
-                      imageUrl: __.user_search[index].profilePictureUrl,
-                      height: 55,
-                      width: 55,
-                    ),
-                    width10,
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          __.user_search[index].username,
-                          style: Theme.of(context).textTheme.labelLarge,
-                        ),
-                        height5,
-                        // Text(
-                        //   __.user_search[index].firstName +
-                        //       ' ' +
-                        //       __.user_search[index].lastName,
-                        //   style: Theme.of(context).textTheme.labelMedium,
-                        // ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            );
+            return _buildUserTile(context, provider, index);
           },
         );
       },
+    );
+  }
+
+  /// Extracted function for better readability
+  Widget _buildUserTile(BuildContext context, SearchProvider provider, int index) {
+    final user = provider.user_search[index];
+
+    return GestureDetector(
+      onTap: () => _navigateToUserProfile(context, user.username),
+      child: Container(
+        height: 65,
+        margin: const EdgeInsets.symmetric(vertical: 5),
+        width: double.infinity, // Ensures full width
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CustomCircularAvatar(
+              imageUrl: user.profilePictureUrl,
+              height: 55,
+              width: 55,
+            ),
+            const SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  user.username,
+                  style: Theme.of(context).textTheme.labelLarge,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// Extracted navigation function for clarity
+  void _navigateToUserProfile(BuildContext context, String username) {
+    Navigator.of(context).pushNamed(
+      UserProfileScreen.routeName,
+      arguments: UserProfileScreenArgs(username: username),
     );
   }
 }
