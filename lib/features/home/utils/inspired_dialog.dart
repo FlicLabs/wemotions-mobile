09377d28e7da14609c3b8130e1bfd8221a646a34
Ayan Mahanta <112053665+Ayan20april@users.noreply.github.com>
2025-02-15 09:53:54 +0530

    Update inspired_dialog.dart

--- lib/features/home/utils/inspired_dialog.dart
+++ lib/features/home/utils/inspired_dialog.dart
@@ -7,117 +7,111 @@ class InspiredDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final home = Provider.of<HomeProvider>(context);
-    final exit = Provider.of<ExitProvider>(context);
+    final home = context.read<HomeProvider>();
+    final exit = context.read<ExitProvider>();
+    final bool isFirstExitValue = isFirstExit ?? false;
+
     return AlertDialog(
       elevation: 0,
-      insetPadding: EdgeInsets.all(15),
+      insetPadding: const EdgeInsets.all(15),
       backgroundColor: Theme.of(context).primaryColor,
       shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(20),
-        ),
+        borderRadius: BorderRadius.all(Radius.circular(20)),
       ),
-      content: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 10, right: 10),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                height5,
-                Text(
-                  isFirstExit == false ? exit_message2 : exit_message1,
-                  // 'This button means you were INSPIRED and you would like to EXIT THE APP, Are you sure you want to exit?',
-                  style: AppTextStyle.normalRegular16.copyWith(
-                    fontWeight: FontWeight.w400,
-                    color: Theme.of(context).indicatorColor,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ],
+      content: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            height5,
+            Text(
+              isFirstExitValue ? exit_message1 : exit_message2,
+              style: AppTextStyle.normalRegular16.copyWith(
+                fontWeight: FontWeight.w400,
+                color: Theme.of(context).indicatorColor,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       actions: [
         Padding(
-          padding: const EdgeInsets.only(left: 10, right: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 10),
           child: Column(
             children: [
-              !isFirstExit!
-                  ? shrink
-                  : Padding(
-                      padding: const EdgeInsets.only(left: 10, right: 10),
-                      child: TransparentButton(
-                        title: 'I FEEL INSPIRED',
-                        onTap: () async {
-                          HapticFeedback.heavyImpact();
-                          if (Platform.isAndroid) {
-                            await home.updateExitCount(id: id);
-                            await exit.initPlayer();
-                            await exit.controller?.play();
-                            Navigator.of(context).pop();
-                            // SystemNavigator.pop();
-                          } else if (Platform.isIOS) {
-                            await exit.initPlayer();
-                            await exit.controller?.play();
-                            Navigator.of(context).pop();
-                            // exit(0);
-                          }
-                        },
-                      ),
-                    ),
-              !isFirstExit! ? shrink : height10,
-              // height20,
-              // height20,
-              isFirstExit!
-                  ? shrink
-                  : Padding(
-                      padding: const EdgeInsets.only(
-                          left: 10, right: 10, bottom: 10),
-                      child: CustomTextButton(
-                        title: 'Okay',
-                        onTap: () async {
-                          await prefs?.setBool('exit', true);
-                          isFirstExit = await prefs?.getBool('exit') ?? false;
-                          Navigator.of(context).pop();
-                        },
-                        fontSize: 13,
-                        buttonColor: Theme.of(context).indicatorColor,
-                        textColor: Theme.of(context).primaryColor,
-                      ),
-                    ),
-              // height5,
-              !isFirstExit!
-                  ? shrink
-                  : Tooltip(
-                      padding: EdgeInsets.all(10),
-                      margin: EdgeInsets.all(40),
-                      showDuration: Duration(seconds: 5),
-                      triggerMode: TooltipTriggerMode.tap,
-                      preferBelow: true,
-                      message: info_message,
-                      child: Padding(
-                        padding: const EdgeInsets.only(
-                          left: 10,
-                          right: 10,
-                          bottom: 10,
-                        ),
-                        child: CustomTextButton(
-                          title: 'ⓘ Learn More',
-                          onTap: null,
-                          fontSize: 13,
-                          buttonColor: Theme.of(context).indicatorColor,
-                          textColor: Theme.of(context).primaryColor,
-                        ),
-                      ),
-                    ),
+              if (isFirstExitValue) _buildInspiredButton(context, home, exit),
+              if (!isFirstExitValue) _buildOkayButton(context),
+              if (isFirstExitValue) _buildLearnMoreButton(context),
               height10,
             ],
           ),
-        )
+        ),
       ],
     );
   }
+
+  /// Button for "I FEEL INSPIRED"
+  Widget _buildInspiredButton(BuildContext context, HomeProvider home, ExitProvider exit) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: TransparentButton(
+        title: 'I FEEL INSPIRED',
+        onTap: () async {
+          HapticFeedback.heavyImpact();
+          if (Platform.isAndroid) {
+            await home.updateExitCount(id: id);
+            await exit.initPlayer();
+            await exit.controller?.play();
+          } else if (Platform.isIOS) {
+            await exit.initPlayer();
+            await exit.controller?.play();
+          }
+          Navigator.of(context).pop();
+        },
+      ),
+    );
+  }
+
+  /// Button for "Okay"
+  Widget _buildOkayButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+      child: CustomTextButton(
+        title: 'Okay',
+        onTap: () async {
+          await prefs?.setBool('exit', true);
+          isFirstExit = await prefs?.getBool('exit') ?? false;
+          Navigator.of(context).pop();
+        },
+        fontSize: 13,
+        buttonColor: Theme.of(context).indicatorColor,
+        textColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+
+  /// Tooltip for "Learn More"
+  Widget _buildLearnMoreButton(BuildContext context) {
+    return Tooltip(
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(40),
+      showDuration: const Duration(seconds: 5),
+      triggerMode: TooltipTriggerMode.tap,
+      preferBelow: true,
+      message: info_message,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+        child: CustomTextButton(
+          title: 'ⓘ Learn More',
+          onTap: null,
+          fontSize: 13,
+          buttonColor: Theme.of(context).indicatorColor,
+          textColor: Theme.of(context).primaryColor,
+        ),
+      ),
+    );
+  }
 }
+
+
