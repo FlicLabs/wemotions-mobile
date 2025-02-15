0f4f1bf553c881318d3c6d357fcc57e023989267
Ayan Mahanta <112053665+Ayan20april@users.noreply.github.com>
2025-02-15 09:31:52 +0530

    Update home_service.dart

--- lib/features/home/domain/services/home_service.dart
+++ lib/features/home/domain/services/home_service.dart
@@ -2,299 +2,143 @@ import 'package:dio/dio.dart';
 import 'package:socialverse/export.dart';
 
 class HomeService {
-  Dio dio = new Dio(BaseOptions(followRedirects: false));
+  final Dio _dio = Dio(BaseOptions(followRedirects: false));
 
-  getFeed(int page, String token) async {
-    print('${API.endpoint}${API.feed}?page=$page');
+  Map<String, String> _getHeaders() {
+    return {'Flic-Token': token ?? ''};
+  }
+
+  Future<dynamic> _handleRequest(Future<Response> Function() request) async {
     try {
-      Response response = await dio.get(
-        '${API.endpoint}${API.feed}?page=$page',
-        options: Options(headers: {'Flic-Token': token}),
-      );
-      ;
-      // print(response.statusCode);
-      // print(response.data);
+      final response = await request();
       return response.data;
-    } on DioError catch (e) {
-      print(e.response?.statusCode);
-      print(e.response?.statusMessage);
-      return e.response?.statusCode;
+    } on DioException catch (e) {
+      print("Error: ${e.response?.statusCode} - ${e.response?.statusMessage}");
+      return e.response?.statusCode ?? 500;
     }
   }
 
-  getSinglePost({required int id}) async {
-    try {
-      Response response = await dio.get(
-        '${API.endpoint}${API.posts}/$id',
-        options: Options(headers: {'Flic-Token': token ?? ''}),
-      );
-      print(response.data);
-      return response;
-    } on DioError catch (e) {
-      print(e.response?.statusMessage);
-      print(e.response?.statusCode);
-      return (e.response?.statusCode);
-    }
+  Future<dynamic> getFeed(int page) async {
+    return _handleRequest(() => _dio.get(
+          '${API.endpoint}${API.feed}?page=$page',
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  postLikeAdd(int id) async {
-    Response response = await dio.post(
-      '${API.endpoint}${API.posts}/$id/upvote',
-      options: Options(headers: {'Flic-Token': token ?? ''}),
-    );
-    print(response.data);
-    return response.data;
+  Future<dynamic> getSinglePost({required int id}) async {
+    return _handleRequest(() => _dio.get(
+          '${API.endpoint}${API.posts}/$id',
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  view({required data}) async {
-    token = prefs?.getString('token');
-    // print('${API.endpoint}${API.view}');
-    Response response = await dio.post(
-      '${API.endpoint}${API.view}',
-      data: data,
-      options: Options(headers: {'Flic-Token': token}),
-    );
-    print(response.data['data']);
-    return response;
+  Future<dynamic> postLikeAdd(int id) async {
+    return _handleRequest(() => _dio.post(
+          '${API.endpoint}${API.posts}/$id/upvote',
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  inspire({required data}) async {
-    token = prefs?.getString('token');
-    print('${API.endpoint}${API.inspire}');
-    Response response = await dio.post(
-      '${API.endpoint}${API.inspire}',
-      data: data,
-      options: Options(headers: {'Flic-Token': token}),
-    );
-    print(response.data['data']);
-    return response;
+  Future<dynamic> postLikeRemove(int id) async {
+    return _handleRequest(() => _dio.delete(
+          '${API.endpoint}${API.posts}/$id/upvote',
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  rating({required data}) async {
-    token = prefs?.getString('token');
-    print('${API.endpoint}${API.rating}');
-    Response response = await dio.post(
-      '${API.endpoint}${API.rating}',
-      data: data,
-      options: Options(headers: {'Flic-Token': token}),
-    );
-    print(response.data);
-    return response;
+  Future<dynamic> blockPost(int id) async {
+    return _handleRequest(() => _dio.post(
+          '${API.endpoint}${API.posts}/$id/block',
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  postLikeRemove(int id) async {
-    Response response = await dio.delete(
-      '${API.endpoint}${API.posts}/$id/upvote',
-      options: Options(headers: {'Flic-Token': token ?? ''}),
-    );
-    print(response.data);
-    return response.data;
+  Future<dynamic> unblockPost(int id) async {
+    return _handleRequest(() => _dio.post(
+          '${API.endpoint}${API.posts}/$id/unblock',
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  blockPost(int id) async {
-    print('${API.endpoint}${API.posts}/$id/block');
-    Response response = await dio.post(
-      '${API.endpoint}${API.posts}/$id/block',
-      options: Options(headers: {'Flic-Token': token}),
-    );
-    print(response.data);
-    return response.data;
+  Future<int> deletePost(int id) async {
+    return await _handleRequest(() => _dio.delete(
+          '${API.endpoint}${API.posts}/$id',
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  unblockPost(int id) async {
-    Response response = await dio.post(
-      '${API.endpoint}${API.posts}/$id/unblock',
-      options: Options(headers: {'Flic-Token': token}),
-    );
-    print(response.data);
-    return response.data;
+  Future<int> deletePostAdmin(int id) async {
+    return await _handleRequest(() => _dio.delete(
+          '${API.endpoint}${API.posts}/$id?api_key=',
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  deletePost(int id) async {
-    print('${API.endpoint}${API.posts}/$id');
-    try {
-      Response response = await dio.delete(
-        '${API.endpoint}${API.posts}/$id',
-        options: Options(headers: {'Flic-Token': token ?? ''}),
-      );
-      print(response.data);
-      print(response.statusCode);
-      return response.statusCode;
-    } catch (e) {
-      print(e);
-      return e;
-    }
+  Future<dynamic> getReplies(int id) async {
+    return _handleRequest(() => _dio.get(
+          '${API.endpoint}${API.posts}/$id/replies',
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  deletePostAdmin(int id) async {
-    try {
-      Response response = await dio.delete(
-        '${API.endpoint}${API.posts}/$id?api_key=',
-        options: Options(headers: {'Flic-Token': token ?? ''}),
-      );
-      print(response.data);
-      print(response.statusCode);
-      return response.statusCode;
-    } catch (e) {
-      print(e);
-      return e;
-    }
+  Future<dynamic> getVotings() async {
+    return _handleRequest(() => _dio.get(
+          '${API.endpoint}get/votings',
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  getReplies(int id, String token) async {
-    print('${API.endpoint}${API.posts}/$id/replies');
-    try {
-      Response response = await dio.get(
-        '${API.endpoint}${API.posts}/$id/replies',
-        options: Options(headers: {'Flic-Token': token}),
-      );
-      ;
-      // print(response.statusCode);
-      // print(response.data);
-      return response.data;
-    } on DioError catch (e) {
-      print(e.response?.statusCode);
-      print(e.response?.statusMessage);
-      return e.response?.statusCode;
-    }
+  Future<dynamic> addVoting({required Map<String, dynamic> data}) async {
+    return _handleRequest(() => _dio.post(
+          '${API.endpoint}${API.posts}/add/votings',
+          data: data,
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  getVotings() async {
-    print('${API.endpoint}get/votings');
-    try {
-      Response response = await dio.get(
-        '${API.endpoint}get/votings',
-        options: Options(headers: {'Flic-Token': token}),
-      );
-      return response.data;
-    } on DioError catch (e) {
-      print(e.response?.statusCode);
-      print(e.response?.statusMessage);
-      return e.response?.statusCode;
-    }
+  Future<dynamic> removeVoting({required Map<String, dynamic> data}) async {
+    return _handleRequest(() => _dio.delete(
+          '${API.endpoint}${API.posts}/remove/votings',
+          data: data,
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  addVoting({required data}) async {
-    print('${API.endpoint}${API.posts}/add/votings');
-    try {
-      Response response = await dio.post(
-        '${API.endpoint}${API.posts}/add/votings',
-        data: data,
-        options: Options(
-          headers: {'Flic-Token': token},
-        ),
-      );
-      return response.data;
-    } on DioError catch (e) {
-      print(e.response?.statusCode);
-      print(e.response?.statusMessage);
-      return e.response?.statusCode;
-    }
+  Future<dynamic> updateVoting(int postId) async {
+    return _handleRequest(() => _dio.get(
+          '${API.endpoint}${API.posts}/$postId',
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  removeVoting({required data}) async {
-    print('${API.endpoint}${API.posts}/remove/votings');
-    try {
-      Response response = await dio.delete(
-        '${API.endpoint}${API.posts}/remove/votings',
-        data: data,
-        options: Options(
-          headers: {'Flic-Token': token},
-        ),
-      );
-
-      return response.data;
-    } on DioError catch (e) {
-      print(e.response?.statusCode);
-      print(e.response?.statusMessage);
-      return e.response?.statusCode;
-    }
+  Future<dynamic> searchUserForTagging(String query) async {
+    return _handleRequest(() => _dio.get(
+          '${API.endpoint}search?type=user&query=$query',
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  updateVoting(int postId) async {
-    print('${API.endpoint}${API.posts}/$postId');
-    try {
-      Response response = await dio.get(
-        '${API.endpoint}${API.posts}/$postId',
-        options: Options(
-          headers: {'Flic-Token': token},
-        ),
-      );
-      return response.data;
-    } on DioError catch (e) {
-      print(e.response?.statusCode);
-      print(e.response?.statusMessage);
-      return e.response?.statusCode;
-    }
+  Future<dynamic> tagUser({required Map<String, dynamic> data}) async {
+    return _handleRequest(() => _dio.post(
+          '${API.endpoint}${API.posts}/tag',
+          data: data,
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  searchUserForTagging(String query) async {
-    print('${API.endpoint}search?type=user&query=$query');
-    try {
-      Response response = await dio.get(
-        '${API.endpoint}search?type=user&query=$query',
-        options: Options(
-          headers: {'Flic-Token': token},
-        ),
-      );
-      return response.data;
-    } on DioError catch (e) {
-      print(e.response?.statusCode);
-      print(e.response?.statusMessage);
-      return e.response?.statusCode;
-    }
+  Future<dynamic> removeTag({required Map<String, dynamic> data}) async {
+    return _handleRequest(() => _dio.delete(
+          '${API.endpoint}${API.posts}/remove/tag',
+          data: data,
+          options: Options(headers: _getHeaders()),
+        ));
   }
 
-  tagUser({required data}) async{
-    print('${API.endpoint}${API.posts}/tag');
-    try {
-      Response response = await dio.post(
-        '${API.endpoint}${API.posts}/tag',
-        data: data,
-        options: Options(
-          headers: {'Flic-Token': token},
-        ),
-      );
-      return response.data;
-    } on DioError catch (e) {
-      print(e.response?.statusCode);
-      print(e.response?.statusMessage);
-      return e.response?.statusCode;
-    }
-  }
-
-  removeTag({required data}) async {
-    print('${API.endpoint}${API.posts}/remove/tag');
-    try {
-      Response response = await dio.delete(
-        '${API.endpoint}${API.posts}/remove/tag',
-        data: data,
-        options: Options(
-          headers: {'Flic-Token': token},
-        ),
-      );
-      return response.data;
-    } on DioError catch (e) {
-      print(e.response?.statusCode);
-      print(e.response?.statusMessage);
-      return e.response?.statusCode;
-    }
-  }
-
-  updateTags(int postId) async {
-    print('${API.endpoint}${API.posts}/$postId');
-    try {
-      Response response = await dio.get(
-        '${API.endpoint}${API.posts}/$postId',
-        options: Options(
-          headers: {'Flic-Token': token},
-        ),
-      );
-      return response.data;
-    } on DioError catch (e) {
-      print(e.response?.statusCode);
-      print(e.response?.statusMessage);
-      return e.response?.statusCode;
-    }
+  Future<dynamic> updateTags(int postId) async {
+    return _handleRequest(() => _dio.get(
+          '${API.endpoint}${API.posts}/$postId',
+          options: Options(headers: _getHeaders()),
+        ));
   }
 }
+
