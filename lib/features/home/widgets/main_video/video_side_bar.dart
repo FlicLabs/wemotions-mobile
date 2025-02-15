6ab44bfd3c42572799091e0e49eb7fd13e877ff3
Ayan Mahanta <112053665+Ayan20april@users.noreply.github.com>
2025-02-15 10:30:32 +0530

    Update video_side_bar.dart

--- lib/features/home/widgets/main_video/video_side_bar.dart
+++ lib/features/home/widgets/main_video/video_side_bar.dart
@@ -5,275 +5,158 @@ class VideoSideBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final home = Provider.of<HomeProvider>(context);
-    final auth = Provider.of<AuthProvider>(context);
+    final home = Provider.of<HomeProvider>(context, listen: false);
+    final auth = Provider.of<AuthProvider>(context, listen: false);
     bool isAdmin = (prefs_username == 'afrobeezy' || prefs_username == 'jack');
-    return Consumer<VideoProvider>(
-      builder: (_, __, ___) {
-        //dynamic count = __.posts[__.index].commentCount;
-        return __.posts.isEmpty
-            ? shrink
-            : Container(
-                height: cs().height(context),
-                alignment: Alignment.bottomRight,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(right: 10),
-                      child: Column(
-                        children: [
-                          Column(
-                            children: [
-                              SideBarItem(
-                                onTap: () {
-                                  if (logged_in == true) {
-                                    if (__.posts[__.index].upvoted) {
-                                      __.posts[__.index].upvoted = false;
-                                      __.posts[__.index].upvoteCount--;
-                                      home.postLikeRemove(
-                                        id: __.posts[__.index].id,
-                                      );
-                                    } else {
-                                      __.posts[__.index].upvoted = true;
-                                      __.posts[__.index].upvoteCount++;
-                                      home.postLikeAdd(
-                                        id: __.posts[__.index].id,
-                                      );
-                                    }
-                                  } else {
-                                    auth.showAuthBottomSheet(context);
-                                  }
-                                },
-                                // onTapDown: (position) {
-                                //   home.getTapPosition(context, position);
-                                // },
-                                // onLongPress: () async {
-                                //   home.showContextMenu(
-                                //     context,
-                                //     id: __.posts[__.index].id,
-                                //     rate_value: __.posts[__.index].ratingCount,
-                                //   );
-                                // },
-                                value: 14,
-                                icon: ShaderMask(
-                                  shaderCallback: (Rect bounds) {
-                                    return __.posts[__.index].upvoted
-                                        ? LinearGradient(
-                                            begin: Alignment.centerLeft,
-                                            end: Alignment.centerRight,
-                                            colors: <Color>[
-                                              Theme.of(context).hintColor,
-                                              Colors.pink,
-                                              Theme.of(context).hintColor,
-                                            ],
-                                            tileMode: TileMode.repeated,
-                                          ).createShader(bounds)
-                                        : const LinearGradient(
-                                            colors: <Color>[
-                                              Colors.white,
-                                              Colors.white
-                                            ],
-                                          ).createShader(bounds);
-                                  },
-                                  child: SvgPicture.asset(
-                                    AppAsset.iclike,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                text: shrink,
-                                // Text(
-                                //   __.posts[__.index].upvoteCount.toString(),
-                                //   style: TextStyle(
-                                //     fontSize: 13,
-                                //     fontWeight: FontWeight.w400,
-                                //     color: Colors.white,
-                                //     fontFamily: 'sofia',
-                                //   ),
-                                // ),
-                              ),
-                              height7,
-                              SideBarItem(
-                                onTap: () {
-                                  showModalBottomSheet(
-                                    constraints: BoxConstraints(
-                                      maxHeight: cs().height(context) / 1.35,
-                                    ),
-                                    isScrollControlled: true,
-                                    shape: const RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.only(
-                                        topRight: Radius.circular(30),
-                                        topLeft: Radius.circular(30),
-                                      ),
-                                    ),
-                                    context: context,
-                                    builder: (context) {
-                                      return FractionallySizedBox(
-                                        heightFactor: 1,
-                                        child: HomeCommentWidget(
-                                          id: __.posts[__.index].id,
-                                        ),
-                                      );
-                                    },
-                                  );
-                                },
-                                value: 14,
-                                icon: SvgPicture.asset(
-                                  AppAsset.iccomment,
-                                  color: Colors.white,
-                                ),
-                                text: shrink,
-                                // Consumer<CommentProvider>(
-                                //   builder: (context, value, child) {
-                                //     if (value.update_count == true) {
-                                //       count += 1;
-                                //       __.posts[__.index].commentCount = count;
-                                //     }
-                                //     return Text(
-                                //       __.posts[__.index].commentCount
-                                //           .toString(),
-                                //       style: TextStyle(
-                                //         fontSize: 13,
-                                //         fontWeight: FontWeight.w400,
-                                //         color: Colors.white,
-                                //         fontFamily: 'sofia',
-                                //       ),
-                                //     );
-                                //   },
-                                // ),
-                              ),
-                              height7,
-                              SideBarItem(
-                                onTap: () async {
-                                  HapticFeedback.mediumImpact();
-                                  bool isUser = __.posts[__.index].username !=
-                                      prefs_username;
 
-                                  showModalBottomSheet(
-                                    context: context,
-                                    backgroundColor: Colors.black,
-                                    shape: const RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(30.0),
-                                        topRight: Radius.circular(30.0),
-                                      ),
-                                    ),
-                                    builder: (context) {
-                                      return ShareSheet(
-                                        isUser: isUser,
-                                        dynamicLink: 'link',
-                                      );
-                                    },
-                                  );
-                                },
-                                value: 0,
-                                icon: Padding(
-                                  padding: EdgeInsets.only(bottom: 3),
-                                  child: Icon(
-                                    UniconsLine.share,
-                                    color: Colors.white,
-                                    size: 22,
-                                  ),
-                                ),
-                                text: shrink,
-                                // Text(
-                                //   'Share',
-                                //   style: TextStyle(
-                                //     fontSize: 13,
-                                //     fontWeight: FontWeight.w400,
-                                //     color: Colors.white,
-                                //     fontFamily: 'sofia',
-                                //   ),
-                                // ),
-                              ),
-                              height7,
-                              SideBarItem(
-                                onTap: () {
-                                  HapticFeedback.mediumImpact();
-                                  if (__.isViewMode) {
-                                    __.isViewMode = false;
-                                  } else {
-                                    __.isViewMode = true;
-                                  }
-                                },
-                                value: 5,
-                                icon: Icon(
-                                  Icons.fullscreen_rounded,
-                                  color: Colors.white,
-                                  size: 25,
-                                ),
-                                text: shrink,
-                                // Text(
-                                //   'View',
-                                //   style: TextStyle(
-                                //     fontSize: 13,
-                                //     fontWeight: FontWeight.w400,
-                                //     color: Colors.white,
-                                //     fontFamily: 'sofia',
-                                //   ),
-                                // ),
-                              ),
-                              // height7,
-                              // SideBarItem(
-                              //   onTap: () async {
-                              //     HapticFeedback.heavyImpact();
-                              //     if (__
-                              //         .videoController(__.index)!
-                              //         .value
-                              //         .isPlaying) {
-                              //       await __.videoController(__.index)!.pause();
-                              //     }
-                              //     home.showInspiredDialog(
-                              //       context,
-                              //       id: __.posts[__.index].id,
-                              //     );
-                              //   },
-                              //   value: 10,
-                              //   icon: Image.asset(
-                              //     AppAsset.icon,
-                              //   ),
-                              //   text: shrink,
-                              // ),
-                              height7,
-                              if (isAdmin) ...[
-                                SideBarItem(
-                                  onTap: () {
-                                    HapticFeedback.lightImpact();
-                                    __.posts[__.index].exitCount += 1;
-                                    __.updateExitCount(
-                                      id: __.posts[__.index].id,
-                                    );
-                                  },
-                                  value: 5,
-                                  icon: Center(
-                                    child: Text(
-                                      '${__.posts[__.index].exitCount}',
-                                      style: TextStyle(
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.w400,
-                                        color: Colors.white,
-                                        fontFamily: 'sofia',
-                                      ),
-                                    ),
-                                  ),
-                                  text: shrink,
-                                ),
-                              ],
-                            ],
-                          ),
-                          SizedBox(
-                            height: __.downloading == true ||
-                                    __.downloadingCompleted
-                                ? cs().height(context) * 0.1
-                                : cs().height(context) * 0.08,
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              );
+    return Selector<VideoProvider, int>(
+      selector: (_, provider) => provider.index,
+      builder: (_, index, __) {
+        final videoProvider = Provider.of<VideoProvider>(context, listen: false);
+        
+        // Ensure posts are not empty and index is valid
+        if (videoProvider.posts.isEmpty || index >= videoProvider.posts.length) {
+          return const SizedBox.shrink();
+        }
+
+        final post = videoProvider.posts[index];
+
+        return Positioned(
+          bottom: 0,
+          right: 10,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              _buildLikeButton(post, videoProvider, home, auth),
+              height7,
+              _buildCommentButton(post, context),
+              height7,
+              _buildShareButton(post, context),
+              height7,
+              _buildViewToggleButton(videoProvider),
+              height7,
+              if (isAdmin) _buildAdminExitCounter(post, videoProvider),
+              SizedBox(
+                height: (videoProvider.downloading || videoProvider.downloadingCompleted)
+                    ? cs().height(context) * 0.1
+                    : cs().height(context) * 0.08,
+              ),
+            ],
+          ),
+        );
       },
     );
   }
+
+  // Like Button
+  Widget _buildLikeButton(Post post, VideoProvider videoProvider, HomeProvider home, AuthProvider auth) {
+    return SideBarItem(
+      onTap: () {
+        if (logged_in) {
+          post.upvoted = !post.upvoted;
+          post.upvoteCount += post.upvoted ? 1 : -1;
+
+          post.upvoted
+              ? home.postLikeAdd(id: post.id)
+              : home.postLikeRemove(id: post.id);
+        } else {
+          auth.showAuthBottomSheet(auth.context);
+        }
+      },
+      value: 14,
+      icon: ShaderMask(
+        shaderCallback: (Rect bounds) {
+          return (post.upvoted
+                  ? LinearGradient(
+                      colors: [Theme.of(auth.context).hintColor, Colors.pink, Theme.of(auth.context).hintColor],
+                    )
+                  : const LinearGradient(colors: [Colors.white, Colors.white]))
+              .createShader(bounds);
+        },
+        child: SvgPicture.asset(AppAsset.iclike, color: Colors.white),
+      ),
+      text: shrink,
+    );
+  }
+
+  // Comment Button
+  Widget _buildCommentButton(Post post, BuildContext context) {
+    return SideBarItem(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+          ),
+          builder: (context) => FractionallySizedBox(
+            heightFactor: 1,
+            child: HomeCommentWidget(id: post.id),
+          ),
+        );
+      },
+      value: 14,
+      icon: SvgPicture.asset(AppAsset.iccomment, color: Colors.white),
+      text: shrink,
+    );
+  }
+
+  // Share Button
+  Widget _buildShareButton(Post post, BuildContext context) {
+    return SideBarItem(
+      onTap: () {
+        HapticFeedback.mediumImpact();
+        showModalBottomSheet(
+          context: context,
+          backgroundColor: Colors.black,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0), topRight: Radius.circular(30.0)),
+          ),
+          builder: (context) => ShareSheet(
+            isUser: post.username != prefs_username,
+            dynamicLink: 'link',
+          ),
+        );
+      },
+      value: 0,
+      icon: Padding(
+        padding: EdgeInsets.only(bottom: 3),
+        child: Icon(UniconsLine.share, color: Colors.white, size: 22),
+      ),
+      text: shrink,
+    );
+  }
+
+  // View Mode Toggle Button
+  Widget _buildViewToggleButton(VideoProvider videoProvider) {
+    return SideBarItem(
+      onTap: () {
+        HapticFeedback.mediumImpact();
+        videoProvider.isViewMode = !videoProvider.isViewMode;
+      },
+      value: 5,
+      icon: Icon(Icons.fullscreen_rounded, color: Colors.white, size: 25),
+      text: shrink,
+    );
+  }
+
+  // Admin Exit Counter
+  Widget _buildAdminExitCounter(Post post, VideoProvider videoProvider) {
+    return SideBarItem(
+      onTap: () {
+        HapticFeedback.lightImpact();
+        post.exitCount += 1;
+        videoProvider.updateExitCount(id: post.id);
+      },
+      value: 5,
+      icon: Center(
+        child: Text(
+          '${post.exitCount}',
+          style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w400, color: Colors.white, fontFamily: 'sofia'),
+        ),
+      ),
+      text: shrink,
+    );
+  }
 }
+
