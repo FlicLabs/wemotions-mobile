c67a8305aa991671abdd33f6929782377c9dd7da
Ayan Mahanta <112053665+Ayan20april@users.noreply.github.com>
2025-02-15 11:24:55 +0530

    Update text_form_decoration.dart

--- lib/core/utils/style/text_form_decoration.dart
+++ lib/core/utils/style/text_form_decoration.dart
@@ -1,34 +1,45 @@
 import 'package:flutter/material.dart';
 
-final textFormFieldDecoration = InputDecoration(
-  hintText: '',
-  filled: true,
-  hintStyle: TextStyle(
-    fontFamily: 'sofia',
-    fontSize: 13,
-    fontWeight: FontWeight.w400,
-    color: Colors.grey.shade500,
-  ),
-  // errorStyle: TextStyle(
-  //   fontFamily: 'sofia',
-  //   fontSize: 12,
-  //   fontWeight: FontWeight.w400,
-  //   color: Colors.white,
-  // ),
-  errorBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(10),
-    borderSide: const BorderSide(color: Colors.red),
-  ),
-  focusedBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(10),
-    borderSide: const BorderSide(color: Colors.transparent),
-  ),
-  enabledBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(10),
-    borderSide: const BorderSide(color: Colors.transparent),
-  ),
-  disabledBorder: OutlineInputBorder(
-    borderRadius: BorderRadius.circular(10),
-    borderSide: const BorderSide(color: Colors.transparent),
-  ),
-);
+InputDecoration textFormFieldDecoration({
+  String hintText = '',
+  Color fillColor = Colors.white,
+  Color borderColor = Colors.grey,
+  Color focusedBorderColor = Colors.blue,
+  Color errorColor = Colors.red,
+  double borderRadius = 10.0,
+}) {
+  return InputDecoration(
+    hintText: hintText,
+    filled: true,
+    fillColor: fillColor,
+    contentPadding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
+    hintStyle: TextStyle(
+      fontFamily: 'sofia',
+      fontSize: 13,
+      fontWeight: FontWeight.w400,
+      color: Colors.grey.shade500,
+    ),
+    errorStyle: const TextStyle(
+      fontFamily: 'sofia',
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: Colors.red,
+    ),
+    errorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(borderRadius),
+      borderSide: BorderSide(color: errorColor),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(borderRadius),
+      borderSide: BorderSide(color: focusedBorderColor, width: 1.5),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(borderRadius),
+      borderSide: BorderSide(color: borderColor.withOpacity(0.3), width: 1),
+    ),
+    disabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(borderRadius),
+      borderSide: BorderSide(color: borderColor.withOpacity(0.1)),
+    ),
+  );
+}
